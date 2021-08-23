a6dcf5c94a0db8deeb3396966e0e286f8ae49372
BenjaminChoron <benjamin.choron@outlook.com>
2021-08-23 11:06:24 +0200

    add Weather methods

--- src/components/Weather.vue
+++ src/components/Weather.vue
@@ -3,8 +3,40 @@
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
-    name: 'Weather'
+    name: 'Weather',
+    data(){
+        return {
+            request: '',
+            weather: undefined,
+            api_key: process.env.VUE_APP_API_KEY,
+            search_url: 'https://api.openweathermap.org/data/2.5/weather?'
+        }
+    },
+    methods: {
+        getWeather(event) {
+            if(event.key == "Enter") {
+                axios
+                .get(`${this.search_url}q=${this.request}&units=metric&appid=${this.api_key}&lang=fr`)
+                .then(response => {
+                    this.weather = response.data
+                    console.log(this.weather);
+                })
+                this.request = '';
+            }
+        },
+        getWeatherClick() {
+            axios
+            .get(`${this.search_url}q=${this.request}&units=metric&appid=${this.api_key}&lang=fr`)
+            .then(response => {
+                this.weather = response.data
+                console.log(this.weather);
+            })
+            this.request = '';
+        }
+    }
 }
 </script>
 

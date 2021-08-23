81bf26724e7f59982f1a1a287e68f74c3f7f40dc
BenjaminChoron <benjamin.choron@outlook.com>
2021-08-23 10:48:18 +0200

    mains.js imports

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import moment from 'moment-timezone'
+moment.tz.setDefault('UTC')
+Vue.prototype.$moment = moment
 
 Vue.config.productionTip = false
 
